3ff65186acbad552fad81885c5ef3df13e68fc30
Jonathan Bydendyk <jonathan@hacklab.co.za>
2025-12-02 20:44:45 +0200

    Added gitignore

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -25,13 +25,16 @@
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
+  Serial.begin(9600);
   pinMode(LED_BUILTIN, OUTPUT);
 }
 
 // the loop function runs over and over again forever
 void loop() {
   digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  Serial.println("ON");
   delay(1000);                       // wait for a second
   digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  Serial.println("OFF");
   delay(1000);                       // wait for a second
 }
